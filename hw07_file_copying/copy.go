cf209a0677071485652cf8846edd750ebeae71d4
pipipi666 <nnm107072@gmail.com>
2025-02-15 15:22:21 +0300

    HW7 is completed

--- hw07_file_copying/copy.go
+++ hw07_file_copying/copy.go
@@ -2,6 +2,10 @@ package main
 
 import (
 	"errors"
+	"io"
+	"os"
+
+	"github.com/cheggaaa/pb/v3"
 )
 
 var (
@@ -10,6 +14,53 @@ var (
 )
 
 func Copy(fromPath, toPath string, offset, limit int64) error {
-	// Place your code here.
-	return nil
+	fileInfo, errInfo := os.Stat(fromPath)
+
+	if errInfo != nil {
+		return ErrUnsupportedFile
+	}
+
+	size := fileInfo.Size()
+
+	if offset > size {
+		return ErrOffsetExceedsFileSize
+	}
+
+	reader, errReader := os.Open(fromPath)
+
+	if errReader != nil {
+		return errReader
+	}
+
+	defer reader.Close()
+
+	reader.Seek(offset, io.SeekStart)
+
+	writer, errWriter := os.Create(toPath)
+
+	if errWriter != nil {
+		return errWriter
+	}
+
+	defer writer.Close()
+
+	var count int64
+
+	if size > limit {
+		count = limit
+	} else {
+		count = size
+	}
+
+	bar := pb.Full.Start64(count)
+	defer bar.Finish()
+	barReader := bar.NewProxyReader(reader)
+
+	_, err := io.CopyN(writer, barReader, limit)
+
+	if errors.Is(err, io.EOF) {
+		return nil
+	}
+
+	return err
 }
--- hw07_file_copying/copy_test.go
+++ hw07_file_copying/copy_test.go
@@ -1,7 +1,43 @@
 package main
 
-import "testing"
+import (
+	"errors"
+	"os"
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
+
+func deleteTestFile(fileName string) {
+	os.Remove(fileName)
+}
 
 func TestCopy(t *testing.T) {
-	// Place your code here.
+	testFileName := "out.txt"
+
+	defer deleteTestFile(testFileName)
+
+	t.Run("Simple test", func(t *testing.T) {
+		err := Copy("testdata/input.txt", testFileName, 0, 100)
+
+		require.NoError(t, err)
+	})
+
+	t.Run("EOF test", func(t *testing.T) {
+		err := Copy("testdata/input.txt", testFileName, 0, 8000)
+
+		require.NoError(t, err)
+	})
+
+	t.Run("Offset exceeds file size test", func(t *testing.T) {
+		err := Copy("testdata/input.txt", testFileName, 8000, 100)
+
+		require.Truef(t, errors.Is(err, ErrOffsetExceedsFileSize), "actual err - %v", err)
+	})
+
+	t.Run("Unsupported file test", func(t *testing.T) {
+		err := Copy("\000", testFileName, 0, 100)
+
+		require.Truef(t, errors.Is(err, ErrUnsupportedFile), "actual err - %v", err)
+	})
 }
